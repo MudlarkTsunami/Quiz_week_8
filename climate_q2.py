902d0e965918d949c71cbeeff344e2b948a776ab
MudlarkTsunami <hjbaker2@hotmail.com>
2023-09-15 20:59:07 +1000

    connected the CSV file to the python file

--- climate_q2.py
+++ climate_q2.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+#Take the data from the CSV file and place it into a pandas dataframe
+CSVDataFrame = pd.read_csv('climate.csv')
 
+#Separate the data
+years = CSVDataFrame['Year']
+co2 = CSVDataFrame['C02']
+temp = CSVDataFrame["Temperature"]
+
+
+#Pre-existing code
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
@@ -16,5 +22,5 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.savefig("co2_temp_2.png")
 
