ed77f7d2a48a437b98400c6c7e53067591e997fd
MudlarkTsunami <hjbaker2@hotmail.com>
2023-09-15 20:39:56 +1000

    connected the database to the python file

--- climate_q1.py
+++ climate_q1.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+import sqlite3
 
+#connecting sqlite to the db file
+conn = sqlite3.connect('climate.db')
+pointer = conn.cursor()
+
+#Getting data from the climatedata table
+pointer.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+wholeData = pointer.fetchall()
+
+#Separating the data and moving it into the variables
+years = [row[0] for row in wholeData]
+co2 = [row[1] for row in wholeData]
+temp = [row[2] for row in wholeData]
+
+#Given plotting code
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
